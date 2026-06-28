02bcdbd363221a93cf17a133fc9f94193c76f438
bygentry <gentrym1@bu.edu>
2026-06-28 12:37:36 -0400

    added afr conversion for KP from yrs -> days

--- Data-Origins_files/setup.R
+++ Data-Origins_files/setup.R
@@ -198,6 +198,7 @@ pan_s <- pan_s %>%
 
 kp_s <- kp_s %>% 
   mutate(
-    IBI = IBI * days_per_month
+    IBI = IBI * days_per_month,
+    AFR = AFR * days_per_year
     # converts months to days
   )
